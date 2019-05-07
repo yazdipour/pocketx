29c8d0fa8712b3f7a69c433de20ef130c1ce84b1
Shahriar <shahriar.yazdipour@outlook.com>
2019-05-07 22:32:00 +0430

    Oops :)

--- Source/PocketX/Handlers/Keys.cs
+++ Source/PocketX/Handlers/Keys.cs
@@ -1,11 +1,11 @@
 ﻿namespace PocketX.Handlers
 {
-    class Keys
+    internal class Keys
     {
         internal static readonly string PocketClientCache = "pocketkey";
-		internal static readonly string Pocket = "78569-dd33fe628fb1fd9911484556";
-		internal static readonly string AppCenter = "c206130c-e533-4cca-ad79-581bcacd7066";
-		internal static readonly string Settings = "settings";
+        internal static readonly string Pocket   ;
+        internal static readonly string AppCenter;
+        internal static readonly string Settings = "settings";
         internal static readonly string Logout = "Logout";
         internal static readonly string MainList = "MainList";
     }
